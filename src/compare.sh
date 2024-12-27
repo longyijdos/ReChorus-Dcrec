3c2e82585307696e55052f4b866718e3de1929c0
longyijdos <46864978+longyijdos@users.noreply.github.com>
2024-12-27 08:50:16 +0800

    Add files via upload

---
+++ src/compare.sh
@@ -0,0 +1,38 @@
+python3 main.py --log_file ./results/MovieLens_1M/Caser/info.log --model_name Caser --path ../data/MovieLens_1M --dataset ML_1MTOPK --epoch 20
+python3 main.py --log_file ./results/Grocery_and_Gourmet_Food/Caser/info.log --model_name Caser --path ../data --dataset Grocery_and_Gourmet_Food --epoch 20
+
+python3 main.py --log_file ./results/MovieLens_1M/Chorus/info.log --model_name Chorus --path ../data/MovieLens_1M --dataset ML_1MTOPK --epoch 20
+python3 main.py --log_file ./results/Grocery_and_Gourmet_Food/Chorus/info.log --model_name Chorus --path ../data --dataset Grocery_and_Gourmet_Food --epoch 20
+
+python3 main.py --log_file ./results/MovieLens_1M/ComiRec/info.log --model_name ComiRec --path ../data/MovieLens_1M --dataset ML_1MTOPK --epoch 20
+python3 main.py --log_file ./results/Grocery_and_Gourmet_Food/ComiRec/info.log --model_name ComiRec --path ../data --dataset Grocery_and_Gourmet_Food --epoch 20
+
+python3 main.py --log_file ./results/MovieLens_1M/ContraRec/info.log --model_name ContraRec --path ../data/MovieLens_1M --dataset ML_1MTOPK --epoch 20
+python3 main.py --log_file ./results/Grocery_and_Gourmet_Food/ContraRec/info.log --model_name ContraRec --path ../data --dataset Grocery_and_Gourmet_Food --epoch 20
+
+python3 main.py --log_file ./results/MovieLens_1M/Dcrec/info.log --model_name Dcrec --path ../data/MovieLens_1M --dataset ML_1MTOPK --epoch 20
+python3 main.py --log_file ./results/Grocery_and_Gourmet_Food/Dcrec/info.log --model_name Dcrec --path ../data --dataset Grocery_and_Gourmet_Food --epoch 20
+
+python3 main.py --log_file ./results/MovieLens_1M/FPMC/info.log --model_name FPMC --path ../data/MovieLens_1M --dataset ML_1MTOPK --epoch 20
+python3 main.py --log_file ./results/Grocery_and_Gourmet_Food/FPMC/info.log --model_name FPMC --path ../data --dataset Grocery_and_Gourmet_Food --epoch 20
+
+python3 main.py --log_file ./results/MovieLens_1M/GRU4Rec/info.log --model_name GRU4Rec --path ../data/MovieLens_1M --dataset ML_1MTOPK --epoch 20
+python3 main.py --log_file ./results/Grocery_and_Gourmet_Food/GRU4Rec/info.log --model_name GRU4Rec --path ../data --dataset Grocery_and_Gourmet_Food --epoch 20
+
+python3 main.py --log_file ./results/MovieLens_1M/KDA/info.log --model_name KDA --path ../data/MovieLens_1M --dataset ML_1MTOPK --epoch 20
+python3 main.py --log_file ./results/Grocery_and_Gourmet_Food/KDA/info.log --model_name KDA --path ../data --dataset Grocery_and_Gourmet_Food --epoch 20
+
+python3 main.py --log_file ./results/MovieLens_1M/NARM/info.log --model_name NARM --path ../data/MovieLens_1M --dataset ML_1MTOPK --epoch 20
+python3 main.py --log_file ./results/Grocery_and_Gourmet_Food/NARM/info.log --model_name NARM --path ../data --dataset Grocery_and_Gourmet_Food --epoch 20
+
+python3 main.py --log_file ./results/MovieLens_1M/SASRec/info.log --model_name SASRec --path ../data/MovieLens_1M --dataset ML_1MTOPK --epoch 20
+python3 main.py --log_file ./results/Grocery_and_Gourmet_Food/SASRec/info.log --model_name SASRec --path ../data --dataset Grocery_and_Gourmet_Food --epoch 20
+
+python3 main.py --log_file ./results/MovieLens_1M/SLRCPlus/info.log --model_name SLRCPlus --path ../data/MovieLens_1M --dataset ML_1MTOPK --epoch 20
+python3 main.py --log_file ./results/Grocery_and_Gourmet_Food/SLRCPlus/info.log --model_name SLRCPlus --path ../data --dataset Grocery_and_Gourmet_Food --epoch 20
+
+python3 main.py --log_file ./results/MovieLens_1M/TiMiRec/info.log --model_name TiMiRec --path ../data/MovieLens_1M --dataset ML_1MTOPK --epoch 20
+python3 main.py --log_file ./results/Grocery_and_Gourmet_Food/TiMiRec/info.log --model_name TiMiRec --path ../data --dataset Grocery_and_Gourmet_Food --epoch 20
+
+python3 main.py --log_file ./results/MovieLens_1M/TiSASRec/info.log --model_name TiSASRec --path ../data/MovieLens_1M --dataset ML_1MTOPK --epoch 20
+python3 main.py --log_file ./results/Grocery_and_Gourmet_Food/TiSASRec/info.log --model_name TiSASRec --path ../data --dataset Grocery_and_Gourmet_Food --epoch 20
